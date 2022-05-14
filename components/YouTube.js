be66321cd11575947edbbaf67c975009cc3f2099
vzvu3k6k <vzvu3k6k@gmail.com>
2022-05-14 15:47:43 +0900

    Embed YouTube

--- components/YouTube.js
+++ components/YouTube.js
@@ -1,3 +1,17 @@
+import { parseYouTubeUrl, buildYouTubeEmbedUrl } from "../lib/youtube";
+
 export function YouTube({ url }) {
-  return <a href={url}>Video</a>;
+  const { videoId, start } = parseYouTubeUrl(url);
+  const embedUrl = buildYouTubeEmbedUrl({ videoId, start });
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={embedUrl}
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  );
 }
